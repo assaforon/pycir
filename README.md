4760655632f494615d2c9c824f9e6d76624049d7
cliffckerr <questionably@gmail.com>
2021-01-02 20:54:36 -0800

    add standard github/python files

---
+++ README.md
@@ -0,0 +1,13 @@
+# pycir
+
+Centered isotonic regression, generic but more tailored to dose-response and dose-finding data.
+
+By Assaf Oron, recoded from R 'cir' package
+
+## Installation
+
+`python setup.py develop`
+
+## Usage
+
+See `examples` folder.
---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup, find_packages
+
+# Get version
+cwd = os.path.abspath(os.path.dirname(__file__))
+version = '0.1'
+
+# Get the documentation
+with open(os.path.join(cwd, 'README.md'), "r") as fh:
+    long_description = fh.read()
+
+CLASSIFIERS = [
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: Other/Proprietary License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Development Status :: 4 - Beta",
+    "Programming Language :: Python :: 3.7",
+]
+
+setup(
+    name="pycir",
+    version=version,
+    author="Assaf Oron",
+    author_email="aoron@idmod.org",
+    description="School reopening in Covasim",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://idmod.org',
+    keywords=["regression"],
+    platforms=["OS Independent"],
+    classifiers=CLASSIFIERS,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+    ],
+)
