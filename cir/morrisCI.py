fe1820f2699dc29bc409ac29df32627714831ec8
Assaf Oron <assaf.oron@gmail.com>
2021-01-03 20:11:52 -0800

    Update morrisCI.py

--- cir/morrisCI.py
+++ cir/morrisCI.py
@@ -26,6 +26,32 @@ def Glower(theta,y,n,j):
     
     return (binom.sf(k=y[j],n=n[j],p=theta)+binom.pmf(k=y[j],n=n[j],p=theta)*Glower(theta=theta,y=y,n=n,j=j-1))
 
+ Confidence intervals for ordered Binomial, based on Morris 1988: recursive algorithm (not exported)
 
+
+### The actual CI algorithms calling the Gupper,Glower utilities
+ 
+def morrisUCL(y,n,halfa=None):
+    
+    y=np.asarray(y).copy()
+    n=np.asarray(n).copy()
+    m=len(y)
+    if(len(n)!=m): stop("Mismatched lengths in Morris.\n")
+    if halfa is None: halfa=0.05
+
+    # weird prep...
+    uout=np.repeat(1,m)
+    a=m-1
+    ### At uppermost doses as long as phat=1, no need for algorithms
+    while(y[a]==n[a] && a>=0) a=a-1
+    if(a<0) return(uout)
+    
+    for b in np.arange(a,0,-1):
+    {
+    	uout[b]=uniroot(function(theta,h,d,alpha,...) h(theta=theta,...)-alpha,interval=c(0,1),
+    		alpha=halfa,j=b,h=Gupper,n=n,y=y)$root
+    }
+    return(uout)
+    
 
 def emptee(): pass
